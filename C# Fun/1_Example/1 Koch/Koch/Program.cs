44f3598f0fef81c5c7dfacb469f32a7984e3f2bf
Sergey Goremykin <GSA05@mail.ru>
2015-01-30 17:29:49 +0300

    C# Fun init

---
+++ C# Fun/1_Example/1 Koch/Koch/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.SmallBasic.Library;
+
+namespace Koch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Turtle.Speed = 10;
+            Turtle.Turn(30);
+            Draw(200, 3);
+            Turtle.Turn(120);
+            Draw(200, 3);
+            Turtle.Turn(120);
+            Draw(200, 3);
+            Turtle.Turn(120);
+        }
+
+        public static void Draw(int len, int n)
+        {
+            if (n==0) Turtle.Move(len);
+            else
+            {
+                Draw(len/3,n-1);
+                Turtle.Turn(-60);
+                Draw(len/3,n-1);
+                Turtle.Turn(120);
+                Draw(len/3,n-1);
+                Turtle.Turn(-60);
+                Draw(len / 3, n - 1);
+            }
+        }
+
+    }
+}
